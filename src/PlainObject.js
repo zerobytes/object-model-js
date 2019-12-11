eb21f4e2444c664b3fdd8495bd7a05ffa3c9a072
Guilherme Ferreira <guilherme.ferreira@zerobyte.app>
2019-12-11 12:54:41 -0300

    plain object creation

---
+++ src/PlainObject.js
@@ -0,0 +1,63 @@
+/**
+ * Can have a simpler object filled by a "data" object, by means of $fill method
+ */
+class PlainObject {
+	constructor(data) {
+		this.$fill(data);
+	}
+}
+
+PlainObject.prototype.$fieldConfig = {};
+
+/**
+ * Applies an object filling based on passed data
+ * @param {object} data object contaning the default properties of this model, and its values
+ */
+PlainObject.prototype.$fill = (data = null) => {
+	data = data instanceof Object ? data : {};
+	if (data.uid) {
+		this.uid = data.uid;
+	}
+	if (data.createdAt) {
+		this.createdAt = data.createdAt;
+	}
+	if (data.createdBy) {
+		this.createdBy = data.createdBy;
+	}
+	//Creates all the public properties for this object
+	//based on the $fieldConfig
+	Object.keys(this.$fieldConfig).map((property) => {
+		let field = this.$fieldConfig[property];
+		this[property] = data[property] || field.defaultValue || '';
+	});
+
+	return this;
+};
+/**
+ * Returns a plain object based on the $fieldConfig properties
+ */
+PlainObject.prototype.$toPlainObject = () => {
+	let plain = {};
+	Object.keys(this.$fieldConfig).map((property) => {
+		plain[property] = this[property];
+	});
+	return plain;
+};
+
+/**
+ * Validates each field of this model,
+ * according to their own validation method
+ */
+PlainObject.prototype.$validate = function() {
+	let errors = {};
+	Object.keys(this.$fieldConfig).find((property) => {
+		let field = this.$fieldConfig[property];
+		let validation = field.validate();
+		if (validation.length) {
+			errors[property] = validation;
+		}
+	});
+	return errors;
+};
+
+exports.default = PlainObject;
--- src/ShapeBase.js
+++ src/ShapeBase.js
@@ -1,25 +1,5 @@
-const objectFill = require('./_functions').objectFill;
+const PlainObject = requre('./PlainObject').default;
 
-/**
- * Can have a simpler object filled by a "data" object, by means of $fill method
- */
-class ShapeBase {
-	constructor() {
-		/**
-		 * Applies an object filling based on passed data
-		 *
-		 * @param {object} data object contaning the default properties of this model, and its values
-		 */
-		this.$fill = (data) => {
-			return objectFill(this, data);
-		};
-	}
-}
-
-/**
- * Field configuration set
- * Accessible from instance of ShapeBase
- */
-ShapeBase.prototype.$fieldConfig = {};
+class ShapeBase extends PlainObject {}
 
 exports.default = ShapeBase;
