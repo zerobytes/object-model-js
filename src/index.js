e7feb020e48b7a33fe3cd008989e2c887d8c0b8f
Guilherme Ferreira <guilherme.ferreira@zerobyte.app>
2019-12-18 01:47:02 -0300

    build

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { default as ModelBase } from './ModelBase';
 export { default as ShapeBase } from './ShapeBase';
-export { default as FT, FieldTypes, FieldType, ComplexTypes } from './FieldTypes';
+export { default as FieldTypes, FieldType, ComplexTypes } from './FieldTypes';
 export { default as Validator } from './Validator';
