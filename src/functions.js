45fdb815926b207d57e0c98718bc9e31103f30e9
brunoteixeirasilva <bruno.teixeira.silva@gmail.com>
2019-12-23 10:52:37 -0300

    UPDATE: version bump on functions added to the dist-exports

---
+++ src/functions.js
@@ -0,0 +1,71 @@
+import * as scrypt from 'scrypt';
+import md5 from 'md5';
+
+const _scryptKeyLength = 32;
+
+const _scryptParams = {
+	N: 8,
+	r: 8,
+	p: 1
+};
+
+/**
+ * Will generate a salt by the maximum of the length specified.
+ * Originally 8 bytes long.
+ *
+ * @param {number} [length] Optional. The final length of the generated salt
+ *
+ * @returns {string} randomly generated salt
+ */
+function generateSalt(length = 8) {
+	var result = '';
+	var characters =
+		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~{}[];:!$%&()*%#@';
+	var charactersLength = characters.length;
+	for (var i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	return result;
+}
+
+/**
+ * Applies a default auth-system-wide passwd hash
+ *
+ * @param {string} pwd The password to encrypt
+ * @param {string} salt The user-level salt to be hashed
+ * @param {object} [scryptParams] Optional. Can pass on specific crypto method params
+ * @param {number} [scryptKeyLength] Optional. Can specify specific key length
+ *
+ * @returns {string} encrypted password for save OR comparison
+ */
+function pwdHash(pwd, salt, scryptParams = _scryptParams, scryptKeyLength = _scryptKeyLength) {
+	const b64Pwd = Buffer.from(pwd),
+		b64Salt = Buffer.from(salt);
+
+	return scrypt.hash(b64Pwd, scryptParams, scryptKeyLength, b64Salt);
+}
+
+/**
+ * Converts a string to Base64
+ *
+ * @param {string} text The string to convert
+ * @param {number} length The max length of the final base64
+ *
+ * @returns {string} base64 hashed string
+ */
+function toBase64(text, length = undefined) {
+	return Buffer.from(text).toString('base64', 0, length);
+}
+
+/**
+ * Makes an MD5 hash from a given string
+ *
+ * @param {string} text To be converted onto md5 hash
+ *
+ * @returns {string} a common md5 lowerCase string
+ */
+function md5Hash(text) {
+	return md5(text);
+}
+
+module.exports = { generateSalt, md5Hash, pwdHash, toBase64 };
